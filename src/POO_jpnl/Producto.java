7ef8ff90bd60e079f00136a24c5c9914cc927131
LABO B 9 <LABO B 9@DESKTOP-RT38SPR>
2026-04-14 18:04:27 -0300

    actalizacion 14/04/26

---
+++ src/POO_jpnl/Producto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POO_jpnl;
+
+/**
+ *
+ * @author LABO B 9
+ */
+public class Producto {
+    
+}
