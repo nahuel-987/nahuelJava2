55e53fe3a61cdd52ca6ccc3f232de8047b8b7091
LABO B 9 <LABO B 9@DESKTOP-RT38SPR>
2026-04-14 17:14:43 -0300

    Actualizacion del 14/4/26

---
+++ src/POO_jpnl/Alumno.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POO_jpnl;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author LABO B 9
+ */
+public class Alumno {
+    String nombre;
+    int nota,notas;
+
+    public Alumno(String nombre, int nota) {
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+    
+    void alumno (){
+        JOptionPane.showInputDialog(null,"Ingrese el Nombre del alumno:");
+        
+        
+        
+        notas = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese la nota del alumno:"));
+    }
+    void aprovacion(){
+        if (notas >= 6){
+            JOptionPane.showMessageDialog(null, "EL alumno aprobo");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "El alumno desaprobo");
+        }
+    }
+        
+}
---
+++ src/POO_jpnl/MainAlumno.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POO_jpnl;
+
+/**
+ *
+ * @author LABO B 9
+ */
+public class MainAlumno {
+    public static void main(String[] args) {
+        
+        Alumno a1Alumno = new Alumno("null", 0);
+        
+        a1Alumno.alumno();
+        
+        a1Alumno.aprovacion();
+        
+    }
+}
